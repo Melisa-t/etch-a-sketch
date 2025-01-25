ee45cde4431b46264518c961cbfef02e7946f951
Melisa-t <melisatormac55@gmail.com>
2025-01-25 03:15:48 +0200

    create main HTML file and create divs using JS

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".container");
+
+
+for (let i = 0; i <= 32; i++) {
+
+}
+
+const grid = document.createElement("div");
+container.appendChild(grid);
+grid.setAttribute("style", "border: black solid 2px; background: white;");
