f540cf3a8cb41fb336fd29dd07c61e7849cf9bc9
Melisa-t <melisatormac55@gmail.com>
2025-01-25 16:31:14 +0200

    create main container and arrange sizes according to value entered after pressing the button

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 const container = document.querySelector(".container");
+const gridButton = document.querySelector(".grid-size-button")
 
 
-for (let i = 0; i <= 32; i++) {
+gridButton.addEventListener("click", () => setGrid())
+
+// dividing i by 100 gives the width and height % of the grids
+
+function setGrid () {
+    cleanGrid()
+    console.log("hi")
+    let gridSize = prompt("Please choose grid size!")
+        for (let x = 0; x <gridSize; x++) {
+            for (let i= 0; i<gridSize; i++) { 
+                    const grid = document.createElement("div");
+                    grid.classList.add("grid");
+                    container.appendChild(grid);
+                    grid.setAttribute("style", `width: ${(100/gridSize)}%; height: ${(100/gridSize)}%`);
+                
+            }
+            }
+
+
+            
 
 }
 
-const grid = document.createElement("div");
-container.appendChild(grid);
-grid.setAttribute("style", "border: black solid 2px; background: white;");
+
+function cleanGrid() {
+    const cleanGridList = document.querySelectorAll(".grid")
+    console.log(cleanGridList)
+    if (!cleanGridList || cleanGridList.length <= 0 )  {
+        return 
+    } 
+    console.log(cleanGridList)
+    for(let y = 0; y < cleanGridList.length; y++) {
+        cleanGridList[y].remove()
+    } 
+
+
+}
